e9c3e36a0ce54ed298e2ac6fa234e6e2facd2582
Jacob Gelman <3182119+ladvoc@users.noreply.github.com>
2026-06-12 16:45:52 -0700

    Cleanup main

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,26 @@
-use eframe::Renderer;
-use parking_lot::deadlock;
-use std::thread;
-use std::time::Duration;
-
 mod app;
 mod data_track;
 mod logo_track;
 mod rpc_ui;
 mod service;
 mod sine_track;
+mod utils;
 mod video_grid;
 mod video_renderer;
-mod utils;
+
+static APP_NAME: &str = "LiveKit Client";
 
 fn main() {
     env_logger::init();
     utils::watch_for_deadlocks();
 
-    eframe::run_native(
-        "LiveKit Client",
-        eframe::NativeOptions {
-            centered: true,
-            renderer: Renderer::Wgpu,
-            ..Default::default()
-        },
-        Box::new(|context| Ok(Box::new(app::AppRoot::new(context)))),
-    )
-    .unwrap();
+    let options = eframe::NativeOptions {
+        centered: true,
+        ..Default::default()
+    };
+
+    let creator: eframe::AppCreator<'_> =
+        Box::new(|context| Ok(Box::new(app::AppRoot::new(context))));
+
+    eframe::run_native(APP_NAME, options, creator).unwrap();
 }
