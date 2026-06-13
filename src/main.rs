4960dff38a34e3f14ae2b8ba3ad915986cd8ce3a
Jacob Gelman <3182119+ladvoc@users.noreply.github.com>
2026-06-12 21:21:56 -0700

    Keep main window on top

--- src/main.rs
+++ src/main.rs
@@ -16,10 +16,13 @@ fn main() {
     env_logger::init();
     utils::watch_for_deadlocks();
 
+    // FIXME: root window has to always be on top to prevent glitches with room windows.
+
     let viewport = egui::ViewportBuilder::default()
         .with_inner_size([280.0, 480.0])
         .with_resizable(false)
-        .with_icon(egui::IconData::default()); // TODO: add custom icon
+        .with_always_on_top()
+        .with_minimize_button(false);
 
     let options = eframe::NativeOptions {
         viewport,
