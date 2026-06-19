7ec720a341599d0b254651c030f91296bf2758a9
Jacob Gelman <3182119+ladvoc@users.noreply.github.com>
2026-06-18 17:56:26 -0700

    Flatten menu bar

--- src/connection/menu_bar.rs
+++ src/connection/menu_bar.rs
@@ -1,5 +1,5 @@
 use crate::connection::ConnCtx;
-use crate::service::AsyncCmd;
+use crate::service::{AsyncCmd, LkService};
 use livekit::SimulateScenario;
 
 /// Top menu bar: Simulate / Publish / Debug actions, all sent to the service.
@@ -12,42 +12,51 @@ impl egui::Widget for TopMenuBar<'_> {
         let service = self.ctx.service;
         egui::MenuBar::new()
             .ui(ui, |ui| {
-                ui.menu_button("Simulate", |ui| {
-                    let scenarios = [
-                        SimulateScenario::SignalReconnect,
-                        SimulateScenario::Speaker,
-                        SimulateScenario::NodeFailure,
-                        SimulateScenario::ServerLeave,
-                        SimulateScenario::Migration,
-                        SimulateScenario::ForceTcp,
-                        SimulateScenario::ForceTls,
-                    ];
-
-                    for scenario in scenarios {
-                        if ui.button(format!("{:?}", scenario)).clicked() {
-                            let _ = service.send(AsyncCmd::SimulateScenario { scenario });
-                        }
-                    }
-                });
-
-                ui.menu_button("Publish", |ui| {
-                    if ui.button("Logo").clicked() {
-                        let _ = service.send(AsyncCmd::ToggleLogo);
-                    }
-                    if ui.button("SineWave").clicked() {
-                        let _ = service.send(AsyncCmd::ToggleSine);
-                    }
-                    if ui.button("DataTrack").clicked() {
-                        let _ = service.send(AsyncCmd::ToggleDataTrack);
-                    }
-                });
-
-                ui.menu_button("Debug", |ui| {
-                    if ui.button("Log stats").clicked() {
-                        let _ = service.send(AsyncCmd::LogStats);
-                    }
-                });
+                simulate_menu(ui, service);
+                publish_menu(ui, service);
+                debug_menu(ui, service);
             })
             .response
     }
+}
+
+fn simulate_menu(ui: &mut egui::Ui, service: &LkService) {
+    const SIMULATE_SCENARIOS: [SimulateScenario; 7] = [
+        SimulateScenario::SignalReconnect,
+        SimulateScenario::Speaker,
+        SimulateScenario::NodeFailure,
+        SimulateScenario::ServerLeave,
+        SimulateScenario::Migration,
+        SimulateScenario::ForceTcp,
+        SimulateScenario::ForceTls,
+    ];
+    ui.menu_button("Simulate", |ui| {
+        for scenario in SIMULATE_SCENARIOS {
+            if ui.button(format!("{:?}", scenario)).clicked() {
+                let _ = service.send(AsyncCmd::SimulateScenario { scenario });
+            }
+        }
+    });
+}
+
+fn publish_menu(ui: &mut egui::Ui, service: &LkService) {
+    ui.menu_button("Publish", |ui| {
+        if ui.button("Logo").clicked() {
+            let _ = service.send(AsyncCmd::ToggleLogo);
+        }
+        if ui.button("SineWave").clicked() {
+            let _ = service.send(AsyncCmd::ToggleSine);
+        }
+        if ui.button("DataTrack").clicked() {
+            let _ = service.send(AsyncCmd::ToggleDataTrack);
+        }
+    });
+}
+
+fn debug_menu(ui: &mut egui::Ui, service: &LkService) {
+    ui.menu_button("Debug", |ui| {
+        if ui.button("Log stats").clicked() {
+            let _ = service.send(AsyncCmd::LogStats);
+        }
+    });
 }
